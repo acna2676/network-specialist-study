dc7cf97e6b699fee3f12cf93ecb167113f267462
ryosuke nakaishi <acna2676@gmail.com>
2022-02-28 10:59:57 +0900

    note:copy postfix conf file

--- doc.md
+++ doc.md
@@ -7,5 +7,26 @@ systemctl enable bind
 
 yum install postfix
 systemctl enable postfix
+[参考](https://centossrv.com/postfix.shtml)
+※
+
+- コマンドは sudo が必要な場合あり
+- echo unknown_user: /dev/null >> /etc/aliases は以下のエラーが出たため保留
+  /etc/aliases: Permission denied
+- postfix テストユーザー:postfix-test
+  pass: お名前.com と同じ
 
 ## Create File server
+
+## EC2 ログイン
+
+```bash
+ssh -i ~/.ssh/network-specialist-study.pem ec2-user@18.181.127.211
+```
+
+2299Qccc
+
+## 疑問点
+
+- Route53 の NS レコードのサーバーを nslookup に指定できた
+  → Route53 の NS レコードのサーバーは権威サーバーのはずだが、フルサービスリゾルバの役割も持っている可能性がある?
--- stack/create_stack.sh
+++ stack/create_stack.sh
@@ -1,6 +1,11 @@
-aws cloudformation create-stack --stack-name network-specialist-study-ec2-rds --template-body file://template.yml --parameters file://parameters.json
-aws cloudformation update-stack --stack-name network-specialist-study-ec2-rds --template-body file://template.yml --parameters file://parameters.json
-aws cloudformation delete-stack --stack-name network-specialist-study-ec2-rds
+aws cloudformation create-stack --stack-name network-specialist-study-ec2 --template-body file://template.yml --parameters file://parameters.json
+aws cloudformation update-stack --stack-name network-specialist-study-ec2 --template-body file://template.yml --parameters file://parameters.json
+aws cloudformation delete-stack --stack-name network-specialist-study-ec2
+
+[Route53]
+aws cloudformation create-stack --stack-name network-specialist-study-route53 --template-body file://Route53/template.yml --parameters file://Route53/parameters.json
+aws cloudformation update-stack --stack-name network-specialist-study-route53 --template-body file://Route53/template.yml --parameters file://Route53/parameters.json
+aws cloudformation delete-stack --stack-name network-specialist-study-route53
 
 [IAM]
 aws cloudformation create-stack --stack-name network-specialist-study-iam --template-body file://IAM/template.yml --capabilities CAPABILITY_NAMED_IAM
